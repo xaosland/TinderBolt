65da58cd2ff6ba33286fda911dc4dea046dcf2ef
xaosland <xaosland@mail.ru>
2024-06-16 20:17:00 +0400

    new project

---
+++ src/main/java/com/javarush/telegram/TinderBoltApp.java
@@ -0,0 +1,33 @@
+package com.javarush.telegram;
+
+import com.javarush.telegram.ChatGPTService;
+import com.javarush.telegram.DialogMode;
+import com.javarush.telegram.MultiSessionTelegramBot;
+import com.javarush.telegram.UserInfo;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.*;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.ArrayList;
+
+public class TinderBoltApp extends MultiSessionTelegramBot {
+    public static final String TELEGRAM_BOT_NAME = "bot-name"; //TODO: добавь имя бота в кавычках
+    public static final String TELEGRAM_BOT_TOKEN = "bot-token"; //TODO: добавь токен бота в кавычках
+    public static final String OPEN_AI_TOKEN = "chat-gpt-token"; //TODO: добавь токен ChatGPT в кавычках
+
+    public TinderBoltApp() {
+        super(TELEGRAM_BOT_NAME, TELEGRAM_BOT_TOKEN);
+    }
+
+    @Override
+    public void onUpdateEventReceived(Update update) {
+        //TODO: основной функционал бота будем писать здесь
+
+    }
+
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new TinderBoltApp());
+    }
+}
